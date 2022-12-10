73432b5611e2f80a569d6f68e47396ebff2f0a90
Mark Mingo <mmingo@luc.edu>
2022-12-09 21:50:43 -0600

    Display

--- src/main/java/Display.java
+++ src/main/java/Display.java
@@ -1,3 +1,19 @@
 public class Display {
     // Empty Display Class
+    public static void display (boolean currentState){
+        // Reject if display is off
+        if(currentState == false){
+            System.out.println("Please turn system on");
+        // Continue operations if display is on
+        }else{
+            // message
+            String message = "Welcome. Ready to take reading";
+            System.out.println(message);
+            // batteryLevel
+            double batteryLevel;
+            // print batteryLevel
+            System.out.println("Current battery level: " + batteryLevel);
+
+        }
+    } 
 }
